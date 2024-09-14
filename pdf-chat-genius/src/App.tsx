1fb2551b74adf3c22bc24ad714d41f9b15f829db
MahendraKotapati <mahendra.kotapati38@gmail.com>
2024-09-14 14:57:19 -0500

    feat: added uplod file

--- pdf-chat-genius/src/App.tsx
+++ pdf-chat-genius/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { Layout } from './components/Layout';
 
 function App() {
   return (
     <div className="App">
-      <div> Hello world </div>
+      <Layout/>
     </div>
   );
 }
