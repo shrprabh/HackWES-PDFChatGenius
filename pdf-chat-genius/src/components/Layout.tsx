c9171d1da3129ec53144680c4ab9c5dbff827a4d
shreyasprabhakar <shrprabh@ttu.edu>
2024-09-14 17:26:32 -0500

    feat(adding layout): using bootstrap

--- pdf-chat-genius/src/components/Layout.tsx
+++ pdf-chat-genius/src/components/Layout.tsx
@@ -1,7 +1,25 @@
-import { ChatWindow } from "./ChatWindow"
+// components/Layout.tsx
+import React from 'react';
+import { Header } from './Header';
+import { ChatWindow } from './ChatWindow';
+import { Footer } from './Footer';
+import { PDFViewer } from './PDFViewer';
 
-export const Layout= () => {
-    return (
-        <ChatWindow/>
-    )    
-}
+export const Layout = () => {
+  return (
+    <div className="container-fluid">
+      <Header />
+      <div className="row">
+        <div className="col-12 col-md-6">
+          <PDFViewer />
+        </div>
+        <div className="col-12 col-md-6">
+          <ChatWindow />
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ pdf-chat-genius/src/components/PDFViewer.tsx
@@ -0,0 +1,4 @@
+export const PDFViewer = () => {
+return(<h1>PDF Viewer</h1>)
+}
+
